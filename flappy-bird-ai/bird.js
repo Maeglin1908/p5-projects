e4f47e5a844acfce6cbd60f0fd3dc587d2a7dd6c
Nilgam <nilgam@proton.me>
2022-09-15 22:22:05 +0200

    AI working
    TODO :
    organize and improve the code
    Add sliders in order to live-modify parameters (pipes sizes, frequency)
    Save and load trained brains

--- flappy-bird-ai/bird.js
+++ flappy-bird-ai/bird.js
@@ -1,11 +1,29 @@
+function mutate(x) {
+    if (random(1) < 0.1) {
+        let offset = randomGaussian() * 0.5;
+        let newx = x + offset;
+        return newx;
+    } else {
+        return x;
+    }
+}
+
 class Bird {
-    constructor() {
+    constructor(brain) {
         this.pos = createVector(birdOffset, height / 2);
         this.width = birdWidth;
         this.height = birdHeight;
         this.velocity = 0;
         this.gravity = 0.4;
         this.score = 0;
+        if (brain instanceof NeuralNetwork) {
+            this.brain = brain.copy();
+            this.brain.mutate(mutate);
+        } else {
+            this.brain = new NeuralNetwork(5, 8, 2);
+            // this.brain.setActivationFunction(ActivationFunction.tanh);
+        }
+        this.fitness = 0;
     }
 
     update() {
@@ -56,5 +74,47 @@ class Bird {
 
     reward() {
         this.score++;
+    }
+
+    think() {
+        // Inputs for neural network :
+        // - Y position
+        // - Y velocity
+        // - X position of closest no-passed pipe
+        // - Y door-top of closest no-passed pipe
+        // - Y door-bottom of closest no-passed pipe
+        // if (this.velocity < 0) {
+        //     return;
+        // }
+        let closest = this.getClosestPipe();
+
+        if (closest) {
+            let pipePartsPos = closest.getPartsPos();
+            let birdY = map(this.pos.y, 0, height, 0, 1);
+            let birdVelocity = map(this.velocity, -15, 15, 0, 1);
+            let closestX = map(closest.pos.x, 0, width, 0, 1);
+            let closestDoorTop = map(pipePartsPos.top.y + pipePartsPos.top.height, 0, height, 0, 1);
+            let closestDoorBottom = map(pipePartsPos.bottom.y, 0, height, 0, 1);
+            let inputs = [birdY, birdVelocity, closestX, closestDoorTop, closestDoorBottom];
+            // console.log(inputs);
+            let decision = this.brain.predict(inputs);
+
+            if (decision[1] > decision[0]) {
+                this.jump();
+            }
+        }
+    }
+
+    getClosestPipe() {
+        let distance = Infinity;
+        let closest = undefined;
+        for (let pipe of pipes) {
+            let pipeDistance = abs(this.pos.x + this.width - pipe.pos.x);
+            if (this.pos.x < pipe.pos.x + pipeWidth && pipeDistance < distance) {
+                distance = pipeDistance;
+                closest = pipe;
+            }
+        }
+        return closest;
     }
 }
